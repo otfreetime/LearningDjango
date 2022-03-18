6ab63a70ca2527c9c95b59678c35142db079b976
Omar Altaezi <omaraltaezi@gmail.com>
2022-03-18 11:57:54 +0300

    Latest Update

--- BasicProject/HelloApp/views.py
+++ BasicProject/HelloApp/views.py
@@ -1,5 +1,26 @@
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+    return render(
+        request,
+        "HelloApp/index.html",  
+        {
+            'title':"Hello Django",
+            'message':"Hello Django!",
+            'content': "on " + now.strftime("%A, %d %B, %Y at %X"),
+        }
+    )   
+      
+
+    #now = datetime.now()
+
+    #html_content = "<html><head><title>Hello, Django</title></head><body>"
+    #html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    #html_content += "</body></html>"
+
+    #return HttpResponse(html_content)
+    ##return HttpResponse("Hello, Django!")
